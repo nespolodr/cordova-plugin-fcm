739dc88e6077e5f264501f7deb712c2c153272b6
Diego Nespolo <diegonespolo@gmail.com>
2019-05-10 12:38:26 -0300

    fix error: cannot find symbol import com.google.firebase.iid.FirebaseInstanceIdService

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -82,4 +82,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
+    
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d("NEW_TOKEN",s);
+    }
 }
